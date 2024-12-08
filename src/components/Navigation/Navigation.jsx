3e3d1f3b355c9335372bdccf9fbf6ef8afe70c38
yasya <bodycamfp@gmail.com>
2024-12-08 23:05:12 +0300

    init

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav className="navigation">
+            <ul>
+                <li><a href="#home">Главная</a></li>
+                <li><a href="#about">О нас</a></li>
+                <li><a href="#contact">Контакты</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
