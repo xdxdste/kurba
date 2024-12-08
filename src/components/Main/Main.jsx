cc3628dc4802f2d9be0e570ddf2b3994a4ea4765
yasya <bodycamfp@gmail.com>
2024-12-09 01:54:01 +0300

    init

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Main.css'
+
+const Main = () => {
+return (
+    <div className="main">
+        <div className="info">
+            <h1>Курбское поселение Ярославского район </h1>
+        </div>
+        <div className="main-content">
+            <h2>Курба — село в Ярославском районе Ярославской области России, одно из самых больших и древних сел
+                ярославской земли. В XV—XVI веках здесь была вотчина князей Курбских, потомков ярославских удельных
+                князей</h2>
+        </div>
+    </div>
+);
+};
+
+export default Main;
