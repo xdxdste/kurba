6f45a67ac665ae4b055babed762146cdaf403262
PiuPyau <nvlad1333@gmail.com>
2025-04-24 17:31:45 +0300

    xd

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -3,15 +3,15 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 // Иконка маркера
-// import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css';
 
-// const customIcon = new L.Icon({
-//   iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-//   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
-//   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-//   iconSize: [25, 41],
-//   iconAnchor: [12, 41],
-// });
+const customIcon = new L.Icon({
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
 
 const MapKurba = () => {
   const mapContainerRef = useRef(null);
@@ -26,7 +26,7 @@ const MapKurba = () => {
     }).addTo(map);
 
     // Добавление маркера
-    L.marker([57.560278, 39.498672]).addTo(map);
+    L.marker([57.560278, 39.498672], {icon:customIcon}).addTo(map);
 
     // Очистка карты при размонтировании компонента
     return () => {
